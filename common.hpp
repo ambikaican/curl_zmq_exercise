e890e10861f57cd69bd2adaf39ca9313a172b562
Alagianambi <abhinav@alagianambismbp.home>
2019-02-27 07:31:03 +0530

    * Corrected the wrong common.hpp checked in previously * Added Prerequestive info

--- common.hpp
+++ common.hpp
@@ -1,36 +1,37 @@
 #include <iostream>
+#include <stdio.h>
 
 
 
-Class Weather_msg
+class weather_msg
 {
 
-	private:
-		int message_code;
+    public:	
+	int message_code;
 #define GET_BY_CITY_NAME 1
 #define GET_BY_CITY_ID 2 
 #define GET_BY_LON_LAT 3
 #define GET_BY_ZIP_CODE 4
-	public:
-		int getCurrentTemp(string city_name)
-		{
-			cout<< "Server: get by city name\n";
-		}
 		
 		int getCurrentTemp(int city_id)
 		{
-			cout<< "Server: get by city ID\n";
+			std::cout<< "Server: get by city ID\n";
 		}
-		int getCurrentTemp(float lon, flot lat)
+		int getCurrentTemp(float lon, float lat)
 		{
-			cout<< "Server: get by LON LAT\n";
+			std::cout<< "Server: get by LON LAT\n";
+		}
+#if 0
+		int getCurrentTemp(string city_name)
+		{
+			std::cout<< "Server: get by city name\n";
 		}
 		int getCurrentTemp(string zip_code, int const)
 		{
-			cout<< "Server: get by ZIP CODE\n";
+			std::cout<< "Server: get by ZIP CODE\n";
 		}
-	
-}
+#endif	
+}weather_msg_t;
 		
 		
 	
